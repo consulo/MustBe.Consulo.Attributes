98dc03645591da0a6c92d9ac21ff8b00cb4f3497
VISTALL <vistall.valeriy@gmail.com>
2014-09-02 21:08:56 +0400

    attribute are replaced

--- CompositeFormattingMethodAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace MustBe.Consulo.Attributes
-{
-    public class CompositeFormattingMethodAttribute : System.Attribute
-    {
-        public CompositeFormattingMethodAttribute(System.String parameterName)
-        {
-        }
-    }
-}
---
+++ InjectLanguageAttributeAttribute.cs
@@ -0,0 +1,9 @@
+namespace MustBe.Consulo.Attributes
+{
+    public class InjectLanguageAttribute : System.Attribute
+    {
+        public InjectLanguageAttribute(System.String language)
+        {
+        }
+    }
+}
